f9ed28289ac19ddabbdc9201f1f1b72280a5425e
quyentb2710 <quyentb2710@gmail.com>
2023-04-08 00:33:02 +0900

    usersテーブルの設定

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,40 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id()
+                ->autoIncrement()
+                ->comment('UserID');
+            // $table->string('name');
+            
+            $table->string('email')
+                ->unique()
+                ->comment('Email');
+
+            $table->string('first_name')
+                ->comment('First Name');
+
+            $table->string('last_name')
+                ->comment('Last Name');
+
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')
+                ->comment('Password');
+
+            $table->tinyInteger('is_admin')
+                ->comment('Admin')
+                ->default(0);
+
+            $table->rememberToken()
+                ->comment('Login Token');
+
+            $table->timestamp('created', $precision = 0)
+                ->comment('Created')
+                ->useCurrent();
+
+            $table->timestamp('modified')
+                ->comment('Modified')
+                ->useCurrent();
+
         });
     }
 
